a8a34fbdf1ca39c0bdca905d2fe30b94cd6ac6ce
mikhail0090025 <52649381+mikhail0090025@users.noreply.github.com>
2023-12-20 10:23:05 +0100

    Encryptor UI modified

--- Encryptor/Encryptor/Form1.Designer.cs
+++ Encryptor/Encryptor/Form1.Designer.cs
@@ -32,11 +32,14 @@
             this.lbl_path = new System.Windows.Forms.Label();
             this.OpenFileDialog = new System.Windows.Forms.OpenFileDialog();
             this.btn_encrypt = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // btn_get_file
             // 
-            this.btn_get_file.Location = new System.Drawing.Point(13, 13);
+            this.btn_get_file.Location = new System.Drawing.Point(9, 37);
             this.btn_get_file.Name = "btn_get_file";
             this.btn_get_file.Size = new System.Drawing.Size(75, 23);
             this.btn_get_file.TabIndex = 0;
@@ -47,7 +50,7 @@
             // lbl_path
             // 
             this.lbl_path.AutoSize = true;
-            this.lbl_path.Location = new System.Drawing.Point(12, 39);
+            this.lbl_path.Location = new System.Drawing.Point(6, 63);
             this.lbl_path.Name = "lbl_path";
             this.lbl_path.Size = new System.Drawing.Size(35, 13);
             this.lbl_path.TabIndex = 1;
@@ -59,7 +62,7 @@
             // 
             // btn_encrypt
             // 
-            this.btn_encrypt.Location = new System.Drawing.Point(15, 55);
+            this.btn_encrypt.Location = new System.Drawing.Point(9, 79);
             this.btn_encrypt.Name = "btn_encrypt";
             this.btn_encrypt.Size = new System.Drawing.Size(75, 23);
             this.btn_encrypt.TabIndex = 2;
@@ -67,19 +70,40 @@
             this.btn_encrypt.UseVisualStyleBackColor = true;
             this.btn_encrypt.Click += new System.EventHandler(this.btn_encrypt_Click);
             // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.btn_encrypt);
+            this.panel1.Controls.Add(this.btn_get_file);
+            this.panel1.Controls.Add(this.lbl_path);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(133, 112);
+            this.panel1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(4, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(124, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Encryption";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.btn_encrypt);
-            this.Controls.Add(this.lbl_path);
-            this.Controls.Add(this.btn_get_file);
+            this.Controls.Add(this.panel1);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Ecnryptor/Decryptor";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -89,6 +113,8 @@
         private System.Windows.Forms.Label lbl_path;
         private System.Windows.Forms.OpenFileDialog OpenFileDialog;
         private System.Windows.Forms.Button btn_encrypt;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
